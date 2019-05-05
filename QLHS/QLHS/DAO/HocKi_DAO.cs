e76c7078064100cd955473ed5be49542c2099a35
ngochiep30051998 <ngochiep30051998@gmail.com>
2019-05-05 11:24:50 +0700

    bổ sung các lớp DTO,DAO còn thiếu

---
+++ QLHS/QLHS/DAO/HocKi_DAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLHS.DAO
+{
+    public class HocKi_DAO
+    {
+        private static HocKi_DAO instance;
+        public static HocKi_DAO Instance
+        {
+            get
+            {
+                if (instance == null) instance = new HocKi_DAO();
+                return instance;
+            }
+            private set
+            {
+                instance = value;
+            }
+        }
+    }
+}
